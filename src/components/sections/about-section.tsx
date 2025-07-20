256084e95b51b3e25c005f66f703c097d5031fa7
Prabhat Kumar <prabhatkumarssm72@gmail.com>
2025-07-20 18:30:26 +0000

    thats the reason i am not able to see the book image because in public/i

--- src/components/sections/about-section.tsx
+++ src/components/sections/about-section.tsx
@@ -186,7 +186,7 @@ export function AboutSection() {
                 </CardHeader>
                 <CardContent className="text-center">
                     <Image 
-                        src="https://placehold.co/400x600.png"
+                        src="/images/innerbattle.jpeg"
                         alt="The Inner Battle Book Cover"
                         width={400}
                         height={600}
